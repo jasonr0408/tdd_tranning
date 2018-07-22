ce0a6a7d7028931c0b0fe8e58031fb1961be455c
jr <jr@gmail.com>
2018-07-22 21:38:22 +0800

    建立controller並驗證參數，這邊應該用正規表示法過濾

---
+++ tests/Unit/Example/BudgetController.php
@@ -0,0 +1,30 @@
+<?php
+namespace Tests\Unit\Example;
+
+use Request;
+use Tests\Unit\Example\BudgetService;
+use Validator;
+
+class BudgetController
+{
+    private $oBudgetService;
+    public function __construct(BudgetService $_oBudgetService)
+    {
+        $this->oBudgetService = $_oBudgetService;
+    }
+    public function getBudget(Request $_oRequest)
+    {
+        $oValidate = Validator::make($_oRequest->all(), [
+            'StartDate' => 'required|string',
+            'EndDate' => 'required|string',
+        ]);
+
+        if ($oValidate->fails()) {
+            return $oValidate->errors()->all();
+        }
+
+        $iBudget = $this->oBudgetService->calculateMoney();
+
+        return response()->json(['result' => true, 'data' => array('Budget' => $iBudget)]);
+    }
+}

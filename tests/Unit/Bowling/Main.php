d1df49ee13c960709151c058de0a3a69d2306b4d
jr <jasonr0408@gmail.com>
2019-01-10 10:11:32 +0800

    案例的註解增加

--- tests/Unit/Bowling/Main.php
+++ tests/Unit/Bowling/Main.php
@@ -1,9 +1,19 @@
 <?php
 namespace Tests\Unit\Bowling;
+// Game有2 methods
+// roll(pins : int)
+// • 每次玩家都一個球。
+// • 參數是擊倒的瓶數。
 
+// score() : int
+// • 在遊戲結束呼叫。
+// • 回傳此次遊戲的最後分數。
+
+// 遊戲有 10 局。每一局1~2次丟球。第10局有2~3次丟球。
+// 分數功能要每次丟球反覆計算。分數計算需依賴下一局。
 class Main
 {
-    public function getBasic()
+    public function score()
     {
         return true;
     }
--- tests/Unit/Bowling/MainTest.php
+++ tests/Unit/Bowling/MainTest.php
@@ -5,19 +5,24 @@ use Mockery;
 use Tests\TestCase;
 use Tests\Unit\Bowling\Main;
 
+// 準備測試兩個案例
+// 1. 測試都沒打中
+// 2. 測試每次都打中一顆
+// 3. 重構
+// 4. 只寫測試案例，測試one Spare
 class MainTest extends TestCase
 {
     public function SetUp()
     {
     }
 
-    public function testBasicTest()
+    public function testScore()
     {
         $bExpect = true;
 
         $oMain = new Main();
 
-        $bActual = $oMain->getBasic();
+        $bActual = $oMain->score();
 
         $this->assertEquals($bExpect, $bActual);
     }
